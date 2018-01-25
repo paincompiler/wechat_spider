0cd5fe1de6d71bd51199d9d032ce3e80b4c0ca01
paincompiler <paincompiler@gmail.com>
2018-01-25 12:36:29 +0800

    chore: test for renyi

--- config.js
+++ config.js
@@ -2,27 +2,27 @@
 
 var config = {
   mongodb: {
-    db: 'mongodb://127.0.0.1:27017/wechat_spider'
+    db: 'mongodb://127.0.0.1:27017/renyi'
   },
   insertJsToNextPage: {
     disable: false,
     jumpInterval: 2,
     minTime: new Date(2017, 0, 1),
-    maxTime: new Date(2017, 11, 1),
+    maxTime: new Date(2019, 0, 1),
     isCrawlExist: false,
     // if true updateNumAt - publishAt
     crawlExistInterval: 1000*60*60*24*3,
     targetBiz: [],
     // 是否保存文章html代码
-    isSavePostContent: false
+    isSavePostContent: true
   },
   insertJsToNextProfile: {
     disable: false,
     jumpInterval: 15,
     // 抓取到minTime 就跳转至下一公众号
-    minTime: new Date(2017, 10, 25),
+    minTime: new Date(2017, 0, 1),
     // 自定义最近多久更新的公众号本次就不用抓取
-    maxUpdatedAt: new Date(2017, 11, 1),
+    maxUpdatedAt: new Date(2019, 2, 1),
     targetBiz: [],
     // 程序开始时间
     beginTime: new Date()
